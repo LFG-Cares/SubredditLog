b591e7cc8d4aa741e2975a48c066031e6a5558af
Sean Callaway <seancallaway@gmail.com>
2022-02-03 12:00:24 -0800

    Fix formatting of imports.

--- subreddit_log/urls.py
+++ subreddit_log/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from entries.views import AddEntryView, EditEntryView, ImportEntriesView, LogView, RulesView, Search, ban_check, health_check
+from entries.views import (AddEntryView, EditEntryView, ImportEntriesView, LogView, RulesView, Search, ban_check,
+                           health_check)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
