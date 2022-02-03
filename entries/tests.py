80309ad19995a5c16a82e92a5c83c36391a5bba6
Sean Callaway <seancallaway@gmail.com>
2022-02-03 12:44:31 -0800

    Add edit entry tests.

--- entries/tests.py
+++ entries/tests.py
@@ -1,5 +1,6 @@
 from constance import config
 from constance.test import override_config
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
@@ -29,13 +30,19 @@ class ProtectedViewsTest(TestCase):
 
     def setUp(self) -> None:
         self.note = 'This guy stinks on ice.'
+        self.mod1 = get_user_model().objects.create_user(username='mod1', password='testing321')
+        self.mod2 = get_user_model().objects.create_user(username='mod2', password='testing123')
+        self.su = get_user_model().objects.create_superuser(username='su', password='super123')
         rule1 = Rule.objects.create(name='Be civil')
-        Entry.objects.create(
+        self.entry = Entry.objects.create(
+            moderator=self.mod1,
             user='thecal714',
             rule=rule1,
             action=Entry.ACTION_PERM_BAN,
             notes=self.note,
         )
+        self.button_code = f'<a class="btn btn-outline-warning" ' \
+                           f'href="{reverse("entry-edit", kwargs={"pk": self.entry.pk})}">Edit</a>'
 
     def test_cannot_see_notes_as_anonymous(self):
         response = self.client.get(reverse('log-view'))
@@ -49,3 +56,34 @@ class ProtectedViewsTest(TestCase):
         with override_config(PUBLIC_MODLOG=False):
             response = self.client.get(reverse('search'))
             self.assertRedirects(response, f"{reverse('account_login')}?next={reverse('search')}")
+
+    def test_cannot_access_edit_as_anonymous(self):
+        response = self.client.get(reverse('entry-edit', kwargs={'pk': self.entry.pk}), follow=True)
+        self.assertRedirects(response,
+                             f"{reverse('account_login')}?next={reverse('entry-edit', kwargs={'pk': self.entry.pk})}")
+
+    def test_cannot_see_edit_button_as_different_nonsuperuser(self):
+        self.client.login(username='mod2', password='testing123')
+        response = self.client.get(reverse('log-view'))
+
+        self.assertNotContains(response, self.button_code)
+
+    def test_can_access_edit_as_mod(self):
+        self.client.login(username='mod1', password='testing321')
+        response = self.client.get(reverse('entry-edit', kwargs={'pk': self.entry.pk}), follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_can_see_edit_button_as_mod(self):
+        self.client.login(username='mod1', password='testing321')
+        response = self.client.get(reverse('log-view'))
+        self.assertContains(response, self.button_code)
+
+    def test_can_access_edit_as_superuser(self):
+        self.client.login(username='su', password='super123')
+        response = self.client.get(reverse('entry-edit', kwargs={'pk': self.entry.pk}), follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_can_see_edit_as_superuser(self):
+        self.client.login(username='su', password='super123')
+        response = self.client.get(reverse('log-view'))
+        self.assertContains(response, self.button_code)
