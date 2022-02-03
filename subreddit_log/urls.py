f9365401e5fae5b6f336ae558b9a36923d778bc7
Sean Callaway <seancallaway@gmail.com>
2022-02-03 11:55:06 -0800

    Add edit button in log view.

--- subreddit_log/urls.py
+++ subreddit_log/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from entries.views import AddEntryView, ImportEntriesView, LogView, RulesView, Search, ban_check, health_check
+from entries.views import AddEntryView, EditEntryView, ImportEntriesView, LogView, RulesView, Search, ban_check, health_check
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LogView.as_view(), name='log-view'),
     path('add_entry', AddEntryView.as_view(), name='entry-create'),
+    path('edit_entry/<int:pk>', EditEntryView.as_view(), name='entry-edit'),
     path('import', ImportEntriesView.as_view(), name='import-entries'),
     path('ban_check', ban_check, name='ban-check'),
     path('search', Search.as_view(), name='search'),
